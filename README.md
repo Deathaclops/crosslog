4ae53f9982a52232193468cae7e887e6337d9089
Deathaclops <deathaclops@gmail.com>
2024-09-20 13:36:07 -0500

    Initial functionality

---
+++ README.md
@@ -0,0 +1,4 @@
+# CrossLog
+A (very) simple wrapper for env_logger and wasm_logger, with
+a println! export for WebAssembly.
+
---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+
+pub use log::{Level, error, warn, info, debug, trace};
+
+#[cfg(all(target_family = "wasm", target_os = "unknown"))]
+pub use WebAssembly::*;
+
+#[cfg(all(target_family = "wasm", target_os = "unknown"))]
+mod WebAssembly {
+
+	pub use web_sys::console::log_1;
+	pub use wasm_bindgen::JsValue;
+
+	#[macro_export]
+	macro_rules! println {
+		($($arg:expr),+) => (
+		{ $crate::log_1 (&$crate::JsValue::from(format!($($arg),+))); }
+		);
+	} // end macro_rules! println
+
+	pub fn init ( level: log::Level ) {
+
+		wasm_logger::init(wasm_logger::Config::new(
+			level
+		)); // end wasm_logger:init
+
+	} // end fn init
+
+} // end mod WebAssembly
+
+#[cfg(not(all(target_family = "wasm", target_os = "unknown")))]
+pub use Native::*;
+
+#[cfg(not(all(target_family = "wasm", target_os = "unknown")))]
+mod Native {
+
+	use env_logger::{Builder, Env};
+	use std::io::Write;
+	pub use std::println;
+
+	pub fn init ( level: log::Level ) {
+		Builder::from_env(Env::default().default_filter_or(
+			level.as_str()
+		)).format(|buf, record| {
+			writeln! ( buf, "{}:{} [{}] {}",
+				record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+				record.level(),
+                record.args()		)
+		}).init();
+	} // end fn init
+
+} // end mod Native
