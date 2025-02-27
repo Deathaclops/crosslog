0edce3a84a90e65e293a04ed4023cc0e61a4217e
Deathaclops <deathaclops@gmail.com>
2025-02-27 14:45:22 -0600

    Added structural changes and debug tools

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
 
-pub use log::{Level, error, warn, info, debug, trace};
+use log::{Level, error, warn, info, debug, trace};
+
+#[cfg(target_arch = "wasm32")]
+
+
+pub mod prelude {
+	pub use log::{Level, error, warn, info, debug, trace};
+	pub use super::println;
+	pub use super::log;
+} // mod prelude
 
 #[cfg(all(target_family = "wasm", target_os = "unknown"))]
 pub use WebAssembly::*;
 
 #[cfg(all(target_family = "wasm", target_os = "unknown"))]
 mod WebAssembly {
+
+	#[cfg(feature = "debug")]
+	pub use console_error_panic_hook;
 
 	pub use web_sys::console::log_1;
 	pub use wasm_bindgen::JsValue;
@@ -16,9 +28,12 @@ mod WebAssembly {
 		{ $crate::log_1 (&$crate::JsValue::from(format!($($arg),+))); }
 		);
 	} // end macro_rules! println
+	pub use println as log;
 
 	pub fn init ( level: log::Level ) {
 
+		#[cfg(feature = "debug")]
+		console_error_panic_hook::set_once();
 		wasm_logger::init(wasm_logger::Config::new(
 			level
 		)); // end wasm_logger:init
@@ -36,6 +51,7 @@ mod Native {
 	use env_logger::{Builder, Env};
 	use std::io::Write;
 	pub use std::println;
+	pub use std::println as log;
 
 	pub fn init ( level: log::Level ) {
 		Builder::from_env(Env::default().default_filter_or(
@@ -50,3 +66,4 @@ mod Native {
 	} // end fn init
 
 } // end mod Native
+
